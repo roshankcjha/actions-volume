f4659eced84dbc019ed1a4ac591beab19610c260
Roshan Krishnachandra Jha <roshanjha819@gmail.com>
2020-03-14 16:18:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const github = require('@actions/github');
 try {
 
   const radius = core.getInput('radius');
-  let vol = (4*3.14*radius*radius*radius)/3;
-  console.log(`The volume of the cone is ${vol}!`);
-  core.setOutput("volume", vol);
+  let volume = (4*3.14*radius*radius*radius)/3;
+  console.log(`The volume of the cone is ${volume}!`);
+  core.setOutput("volume", volume);
 
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
